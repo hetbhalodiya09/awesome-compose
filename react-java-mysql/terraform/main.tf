69d74c52c89bc809b0c9d30c06a03ac9284a5b8d
AttraTechnologies <ssharma@attratechnologies.com>
2026-05-19 11:30:15 +0530

    Add Kubernetes and Terraform configuration

---
+++ react-java-mysql/terraform/main.tf
@@ -0,0 +1,85 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.31"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      min_size     = 2
+      max_size     = 3
+
+      instance_types = ["t3.medium"]
+    }
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-south-1a", "ap-south-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
+resource "aws_security_group" "rds" {
+  name_prefix = "rds-sg-"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [module.vpc.vpc_cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_subnet_group" "main" {
+  name       = "eks-rds-subnet-group"
+  subnet_ids = module.vpc.private_subnets
+}
+
+resource "aws_db_instance" "mysql" {
+  identifier             = "eks-mysql"
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t4g.micro"
+  allocated_storage      = 20
+  storage_type           = "gp3"
+
+  db_name                = "appdb"
+  username               = "admin"
+  password               = "MyStrongPassword123"
+
+  db_subnet_group_name   = aws_db_subnet_group.main.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+
+  publicly_accessible    = false
+  multi_az               = false
+  skip_final_snapshot    = true
+  deletion_protection    = false
+}
